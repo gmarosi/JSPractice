6c89d1f41f7c2f27bd78a571b5c26053146946c9
Marosi Gergely <gmarosi03@gmail.com>
2024-03-03 19:04:30 +0100

    refactor: make Ball child of Shape

--- bouncyBalls/main.js
+++ bouncyBalls/main.js
@@ -27,13 +27,10 @@ class Shape {
     }
 }
 
-class Ball {
+class Ball extends Shape {
 
    constructor(x, y, velX, velY, color, size) {
-      this.x = x;
-      this.y = y;
-      this.velX = velX;
-      this.velY = velY;
+      super(x, y, velX, velY);
       this.color = color;
       this.size = size;
    }
