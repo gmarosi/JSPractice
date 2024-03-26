cd70d5d64e90ebe185933a34b6162bb04a9ed1f2
Marosi Gergely <gmarosi03@gmail.com>
2024-03-26 14:40:08 +0100

    refactor: increase image size and variety

--- gallery/src/routes/+page.js
+++ gallery/src/routes/+page.js
@@ -4,8 +4,8 @@ export async function load() {
     const imgs = [];
 
     for(let i = 0; i <= 10; i++) {
-        const rand = Math.floor(Math.random() * 100);
-        const resp = await fetch(`https://picsum.photos/seed/${rand}/500`);
+        const rand = Math.floor(Math.random() * 1000);
+        const resp = await fetch(`https://picsum.photos/seed/${rand}/1000`);
 
         if(!resp.ok) {
             throw error(500, 'Image fetch request failed');
