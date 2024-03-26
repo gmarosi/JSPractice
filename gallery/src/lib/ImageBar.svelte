b1df80b18cbaad9431f06bb810b7b7f199a0c3b3
Marosi Gergely <gmarosi03@gmail.com>
2024-03-26 13:48:58 +0100

    feat: implement basic ImageBar

--- gallery/src/lib/ImageBar.svelte
+++ gallery/src/lib/ImageBar.svelte
@@ -1,7 +1,26 @@
 <script>
-    //export let selected;
+    export let imgs;
 </script>
 
 <div class="thumbbar">
-    <h2>placeholder</h2>
+    {#each imgs as img}
+        <img src={img} alt="randomly generated" />
+    {/each}
 </div>
+
+<style>
+    .thumbbar {
+        display: flex;
+        flex-wrap: nowrap;
+        width: 100%;
+        overflow-y: hidden;
+        overflow-x: auto;
+    }
+
+    img {
+        flex: 1;
+        max-width: 250px;
+        max-height: 250px;
+        border: 2px solid black;
+    }
+</style>
--- gallery/src/routes/+page.svelte
+++ gallery/src/routes/+page.svelte
@@ -2,15 +2,24 @@
     import ImageBar from '$lib/ImageBar.svelte';
 
     export let data;
-    const imgURL = URL.createObjectURL(data.imgs[0]);
+    const imgs = data.imgs.map((img) => URL.createObjectURL(img));
 
     let selected;
 </script>
 
 <h1>Image Gallery</h1>
 
-<div class="largeimage">
-    <img src={imgURL} alt="randomly generated image">
+<div class="grid">
+    <div class="largeimage">
+        <img src={imgs[0]} alt="randomly generated" />
+    </div>
+
+    <ImageBar {imgs}/>
 </div>
 
-<ImageBar {selected}/>
+<style>
+    .grid {
+        display: grid;
+        grid-template-rows: 2fr 1fr;
+    }
+</style>
